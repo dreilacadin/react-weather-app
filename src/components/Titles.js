e7b66e31187d88b654c82b96d067edec1f40fadf
Drei Lacadin <lacadin.drei@gmail.com>
2019-02-04 02:49:38 +0800

    first commit

---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">React Weather</h1>
+    <p className="title-container__subtitle">Find out temperature, Conditions and more...</p>
+  </div>
+);
+export default Titles;
