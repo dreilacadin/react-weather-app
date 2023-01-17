78b98511143aecc0ba9bb3fff427834d2dd720c7
Michael Andre John T. Lacadin <38705944+dreilacadin@users.noreply.github.com>
2023-01-17 07:07:17 +0000

    re-deploying to netflify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import Titles from "./components/Titles";
-import Form from "./components/Form";
-import Weather from "./components/Weather";
+import React from "react"
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
 
-const API_KEY = "908ad75f36452c11ff4306cd53162218";
+const API_KEY = "908ad75f36452c11ff4306cd53162218"
 
 class App extends React.Component {
   state = {
@@ -15,12 +15,14 @@ class App extends React.Component {
     error: undefined
   }
   getWeather = async (e) => {
-    e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    if (city && country && data.cod === "404"){
+    e.preventDefault()
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    )
+    const data = await api_call.json()
+    if (city && country && data.cod === "404") {
       this.setState({
         temperature: undefined,
         city: undefined,
@@ -28,9 +30,8 @@ class App extends React.Component {
         humidity: undefined,
         description: undefined,
         error: "Invalid location"
-      });
-    } else if (city && country){
-      console.log(data);
+      })
+    } else if (city && country) {
       this.setState({
         temperature: data.main.temp,
         city: data.name,
@@ -38,7 +39,7 @@ class App extends React.Component {
         humidity: data.main.humidity,
         description: data.weather[0].description,
         error: ""
-      });
+      })
     } else {
       this.setState({
         temperature: undefined,
@@ -47,10 +48,10 @@ class App extends React.Component {
         humidity: undefined,
         description: undefined,
         error: "Please enter valid city and country."
-      });
+      })
     }
   }
-  render(){
+  render() {
     return (
       <div>
         <div className="wrapper">
@@ -61,14 +62,14 @@ class App extends React.Component {
                   <Titles />
                 </div>
                 <div className="col-xs-7 form-container">
-                  <Form getWeather={this.getWeather}/>
+                  <Form getWeather={this.getWeather} />
                   <Weather
-                  temperature={this.state.temperature}
-                  city={this.state.city}
-                  country={this.state.country}
-                  humidity={this.state.humidity}
-                  description={this.state.description}
-                  error={this.state.error}
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
                   />
                 </div>
               </div>
@@ -76,11 +77,8 @@ class App extends React.Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
-};
-
-
-
+}
 
 export default App
